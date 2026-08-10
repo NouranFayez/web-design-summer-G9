4aa3ffc814b493e0e86f7c0b907b48f3b962e2ba
NouranFayez <nouran_ahmed269@hotmail.com>
2026-08-10 10:32:24 +0300

    session12

---
+++ session12/js/script.js
@@ -0,0 +1,140 @@
+// // function nameOfFunction(){
+//     logic
+
+// }
+
+// nameOfFunction()
+
+
+// function sum(number1 , number2 , number3 = 200 ){
+//     console.log(number3)
+//     var num1 = number1
+//     var num2 = number2
+//     var result = num1+num2+number3
+//     return result
+//     console.log(result)
+// }
+
+// var output = sum(100 , 50 , 70 )
+
+
+// console.log(output)
+
+
+// function checkUser(){
+//     var p = prompt("enter your name")
+//     if(p == 'sara') console.log("welcome sara");
+//     else console.log('invalid user');
+
+// }
+
+// checkUser()
+
+
+// function looping(startNum , endNum = 30){
+//     if(endNum == 30){
+//         for(var i = startNum ; i <= endNum ; i++){
+//             console.log(i)
+//         }
+
+//     }
+//     else{
+//        console.log('invalid') 
+//        return null
+//     } 
+
+
+// }
+
+// var loop = looping(10 , 20)
+
+// console.log(loop)
+
+
+// function statment
+// function getName() {
+//     var p = prompt("enter your name")
+//     if (p == "" || p == null) {
+//         return "please enter user name"
+        
+
+//     }
+//     else if (p == 'zein') {
+//         return 'welcome zein'
+//     }
+//     else {
+//         return 'invalid name'
+//     }
+   
+// }
+
+// var name = getName()
+// // string 
+// // empty string
+// // null
+
+// console.log(name);
+
+
+
+// function experession
+// var getName = function(){
+//        var p = prompt("enter your name")
+//     if (p == "" || p == null) {
+//         return "please enter user name"
+        
+
+//     }
+//     else if (p == 'zein') {
+//         return 'welcome zein'
+//     }
+//     else {
+//         return 'invalid name'
+//     }
+// }
+
+// console.log(getName())
+
+
+// arrow function 
+// var getName = ()=>{
+   
+//        var p = prompt("enter your name")
+//     if (p == "" || p == null) {
+//         return "please enter user name"
+        
+
+//     }
+//     else if (p == 'zein') {
+//         return 'welcome zein'
+//     }
+//     else {
+//         return 'invalid name'
+//     }
+// }
+
+// console.log(getName())
+
+var firstName = (num)=>
+    {
+        return num > 20
+    } 
+
+
+
+var firstName = (num)=> num > 20
+
+
+
+console.log(firstName(10))
+
+
+
+
+
+
+
+
+
+
+
