e64344bd47637d860953d88f0cdb6a647be15d42
NouranFayez <nouran_ahmed269@hotmail.com>
2026-08-11 13:32:16 +0300

    session13

--- session13/js/script.js
+++ session13/js/script.js
@@ -2,14 +2,16 @@
 
 var h2Elements = document.getElementsByTagName("h2")
 
-console.log(h2Elements)
-console.log(h2Elements[2].innerText)
-h2Elements[2].innerText = "Javascript"
-console.log(h2Elements[2].innerText)
+if (h2Elements.length > 0) {
+    console.log(h2Elements)
+    console.log(h2Elements[2].innerText)
+    h2Elements[2].innerText = "Javascript"
+    console.log(h2Elements[2].innerText)
+}
 
 
 var elements = document.getElementsByClassName('text-dark')
-console.log(elements)
+// console.log(elements)
 
 // Web design Summer G9
 
@@ -32,20 +34,22 @@ for (var i = 0; i < elements.length; i++) {
 // user.addressList.office
 
 
-var h1 = document.getElementById('h1_element')
-console.log(h1)
-// document.getElementById('h1_element').style.color = "teal"
+if (document.getElementById('h1_element')) {
+    var h1 = document.getElementById('h1_element')
+    console.log(h1)
+    // document.getElementById('h1_element').style.color = "teal"
 
-// h1.style.color = "red"
+    // h1.style.color = "red"
 
-h1.classList.add("card", "m-auto")
+    h1.classList.add("card", "m-auto")
 
-h1.classList.remove('border')
+    h1.classList.remove('border')
 
-// classList.toggle() ==> search
+    // classList.toggle() ==> search
 
-// document.querySelector() ==> search
-// document.querySelectorAll() ==> search
+    // document.querySelector() ==> search
+    // document.querySelectorAll() ==> search
+}
 
 function changeImg() {
     var img = document.getElementById('card-img')
@@ -54,8 +58,8 @@ function changeImg() {
     img.src = "images/2.jpg"
 }
 
-function handleClick(element){
-        console.log(element.src)
+function handleClick(element) {
+    console.log(element.src)
 }
 
 // var userNameInput = document.getElementById('userName')
@@ -63,9 +67,74 @@ function handleClick(element){
 // userNameInput.addEventListener("input" , ()=>{
 //             // console.log('typing......')
 //             // console.log(userNameInput.value)
-//           userNameInput.nextElementSibling.innerText = userNameInput.value  
+//           userNameInput.nextElementSibling.innerText = userNameInput.value
 
 // })
+
+
+
+///////////////////////////// About page Js 
+
+var users = []
+
+if (document.getElementById('userForm')) {
+    var form = document.getElementById('userForm')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        // console.log(e)
+        // console.log('form is submitting')
+        // console.log(e.target[0].value)
+        // console.log(e.target[1].value)
+        var obj = {
+            name: e.target[0].value,
+            email: e.target[1].value
+        }
+        users.push(obj)
+        // users.push({
+        //     name: e.target[0].value,
+        //     email: e.target[1].value
+        // })
+        console.table(users)
+
+        showUser()
+    })
+
+
+
+    // form.addEventListener('submit' , (event)=>{
+
+    // })
+
+
+}
+
+
+function showUser() {
+    var cardsCont = document.getElementById('cards-container')
+
+    var divElement = document.createElement('div')
+    var h4Name = document.createElement('h4')
+    var h4Email = document.createElement('h4')
+
+    h4Name.innerText = "Nouran"
+    h4Email.innerText = "Nouran@email.com"
+
+    divElement.appendChild(h4Name)
+    divElement.appendChild(h4Email)
+
+    divElement.classList.add('alert', 'alert-primary')
+
+    cardsCont.appendChild(divElement)
+
+    console.log(divElement)
+}
+
+
+//   <!-- <div class="alert alert-primary">
+//                     <h4>Name : Nouran</h4>
+//                     <h4>Email : Nouran@email.com</h4>
+//                 </div> -->
+
 
 
 
